3419819494d0ced7d542eadf0e554525ea4f73f4
Elavarasan <contact@elavarasan.me>
2024-05-05 15:30:26 +0530

    update - 28/04

--- frontend/src/helpers/strings.tsx
+++
@@ -1,9 +0,0 @@
-const Strings: any = {
-    notifications: {
-        attention: 'Atenção!',
-        phishingProbability: (probability: string) => `Esse site possui ${probability} de chance de ser falso`
-    },
-    classificationError: 'Não foi possível classificar este site.'
-}
-
-export default Strings
